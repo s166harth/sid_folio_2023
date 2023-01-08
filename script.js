fef54593dccd2187cf614f548879677c89af62d0
s166harth <siddharthsagar2019@gmail.com>
2023-01-08 22:04:38 +0530

    working projects section

--- script.js
+++ script.js
@@ -127,6 +127,55 @@ const projects = [
         gif: "https://user-images.githubusercontent.com/56957437/159137865-e903e1f5-687c-412a-86b4-05791173b57a.gif",
         description: "This is my attemp to make a text based social media application using MongoDB for database",
         link: "https://github.com/s166harth/sid-share"
+    },
+    {
+      title: "spotify-UI-clone",
+      tags: "#html #css #JS",
+      description: "I tried to make a spotify clone for my coding ninja's project",
+      link:"https://github.com/s166harth/CN_Major_1",
+      gif: "https://user-images.githubusercontent.com/56957437/211206402-22a17b29-ea61-4faf-aa98-803479648f52.png",
+    },
+    {
+      title: "IMDb Clone",
+      tags:"#html #css #JS",
+      description: "A complete IMDb clone using OMDb API for search functionality and Wishlist making",
+      gif: "https://user-images.githubusercontent.com/56957437/211206827-3fd1dbc4-110a-4296-91f9-84efe2a495b4.png",
+      link: "https://github.com/s166harth/CN_SKILLTEST_FRONTEND_2",
+    },
+    {
+      title: "Stock-Screener",
+      tags: "#Python #streamlit",
+      description:"A terminal to check stock prices with various indicators on charts",
+      gif:"https://user-images.githubusercontent.com/56957437/158430160-a58ee196-5303-445a-a0af-b0f38856c025.gif",
+      link:"https://github.com/s166harth/stock-screener",
+    },
+    {
+      title:"Folio",
+      tags:"#reactJS #html #css",
+      description:"This is my old portfolio website, it's still online, feel free to check it out ;)",
+      gif:"https://user-images.githubusercontent.com/56957437/211207168-537caba5-038d-4808-8731-a162a5a6cff5.png",
+      link:"https://siddharth2022.netlify.app/",
+    },
+    {
+      title:"Quant Trading Terminal",
+      tags:"#ReactJS #Python #MaterialUI #NodeJS",
+      description:"Our B.Tech project which makes quant trading simple for normal people by providing them pre-made scripts",
+      gif:"https://user-images.githubusercontent.com/56957437/211207417-5f39daf9-e1c1-40ea-ae49-01552d67b2f2.png",
+      link:"https://github.com/s166harth/Quant-trading-terminal",
+    },
+    {
+      title:"Blogging Site",
+      tags:"#django #MySQL",
+      description:"A very simple blogging site made using Django framework",
+      gif:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.tvabjwNjlbY0mkD9OQbLZAHaE8%26pid%3DApi&f=1&ipt=617e2c38dfc44673a1daf3fc5631a6b4de7b2799e23febce045f2be6d0091fc9&ipo=images",
+      link:"https://github.com/s166harth/sidblog"
+    },
+    {
+      title:"Crypto-currency price analysis",
+      tags:"#python #machinelearning",
+      description:"My 4th sem project which is a bunch of ML algorithms to predict prices of cryptocurrencies",
+      gif:"https://assets.entrepreneur.com/content/3x2/2000/20181220164101-GettyImages-930264238.jpeg",
+      link:"https://github.com/s166harth/finance-project"
     }
 ]
 
